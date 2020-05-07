a27f0b3137e767d0add99d4f5b1f75d56c249554
marble0117 <guitarmarble0117@gmail.com>
2020-05-07 10:15:36 +0900

    updated

--- models/longae.py
+++ models/longae.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class LoNGAE(nn.Module):
-    def __init__(self, data, nhid=256, latent_dim=128):
+    def __init__(self, data, nhid=32, latent_dim=16):
         super(LoNGAE, self).__init__()
         self.W1 = nn.Parameter(torch.FloatTensor(data.num_nodes + data.num_features, nhid))
         self.W2 = nn.Parameter(torch.FloatTensor(nhid, latent_dim))
@@ -12,6 +12,12 @@ class LoNGAE(nn.Module):
         self.b2 = nn.Parameter(torch.FloatTensor(latent_dim))
         self.b3 = nn.Parameter(torch.FloatTensor(nhid))
         self.b4 = nn.Parameter(torch.FloatTensor(data.num_nodes + data.num_features))
+
+        N = data.features.size(0)
+        E = data.adjmat.sum()
+        pos_weight = (N * N - E) / E
+        self.weight_mat = torch.where(data.adjmat > 0, pos_weight, torch.tensor(1.))
+        self.norm = (N * N) / ((N * N - E) * 2)
 
     def reset_parameters(self):
         nn.init.xavier_uniform_(self.W1, gain=1.414)
@@ -22,8 +28,12 @@ class LoNGAE(nn.Module):
         self.b4.data.fill_(0)
 
     def recon_loss(self, data, output):
-        true_x = torch.cat([data.adjmat, data.features], dim=1)
-        return F.binary_cross_entropy(output['recon_x'], true_x)
+        recon = output['recon_x']
+        recon_a = recon[:, :data.num_nodes]
+        recon_x = recon[:, data.num_nodes:]
+        recon_a_loss = self.norm * F.binary_cross_entropy(recon_a, data.adjmat, weight=self.weight_mat)
+        recon_x_loss = F.binary_cross_entropy(recon_x, data.features)
+        return recon_a_loss + recon_x_loss
 
     def loss_function(self, data, output):
         return self.recon_loss(data, output)
