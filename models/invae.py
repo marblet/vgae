2e007b679214f5c97c8091bb292d66e0c3fe40ca
marblet <guitarmarble0117@gmail.com>
2020-07-08 18:17:53 +0900

    reconstruct x and a

--- models/invae.py
+++ models/invae.py
@@ -3,22 +3,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models import GCNConv, Encoder, reparameterize
+from models import Encoder, reparameterize
 
 
 class INVAE(nn.Module):
-    def __init__(self, data, nhid=32, latent_dim=16):
+    def __init__(self, data, nhid=32, latent_dim=16, dropout=0.5):
         super(INVAE, self).__init__()
         self.encoder = Encoder(data, nhid, latent_dim)
-        self.decoder = InverseDecoder(data, latent_dim, nhid)
+        self.decoder = OmitDecoder(data, latent_dim, nhid, dropout)
 
     def reset_parameters(self):
         self.encoder.reset_parameters()
         self.decoder.reset_parameters()
 
     def recon_loss(self, data, output):
-        adj_recon = output['adj_recon']
-        return data.norm * F.binary_cross_entropy_with_logits(adj_recon, data.adjmat, pos_weight=data.pos_weight)
+        recon_adj = output['recon_adj']
+        adj_loss = data.norm * F.binary_cross_entropy_with_logits(recon_adj, data.adjmat, pos_weight=data.pos_weight)
+        recon_feat = output['recon_feat']
+        feat_loss = F.mse_loss(recon_feat, data.features)
+        return adj_loss + feat_loss
 
     def loss_function(self, data, output):
         recon_loss = self.recon_loss(data, output)
@@ -30,39 +33,30 @@ class INVAE(nn.Module):
     def forward(self, data):
         mu, logvar = self.encoder(data)
         z = reparameterize(mu, logvar, self.training)
-        recon_feat = self.decoder(z)
-        return {'recon_feat': recon_feat, 'z': z, 'mu': mu, 'logvar': logvar}
+        recon_adj, recon_feat = self.decoder(z)
+        return {'recon_adj': recon_adj, 'recon_feat': recon_feat, 'z': z, 'mu': mu, 'logvar': logvar}
 
 
-def inverse(adj):
-    adj = adj.to_dense().numpy()
-    return torch.tensor(np.linalg.pinv(adj))
-
-
-class DenseGCNConv(GCNConv):
-    def __init__(self, in_features, out_features):
-        super(DenseGCNConv, self).__init__(in_features, out_features)
-
-    def forward(self, x, adj):
-        x = torch.matmul(x, self.weight)
-        x = torch.matmul(adj, x)
-        if self.bias is not None:
-            x += self.bias
-        return x
-
-
-class InverseDecoder(nn.Module):
-    def __init__(self, data, latent_dim, nhid):
-        super(InverseDecoder, self).__init__()
-        self.gc1 = DenseGCNConv(latent_dim, nhid)
-        self.gc2 = DenseGCNConv(nhid, data.num_features)
-        self.inv_adj = inverse(data.adj)
+class OmitDecoder(nn.Module):
+    def __init__(self, data, latent_dim, nhid, dropout):
+        super(OmitDecoder, self).__init__()
+        self.feat_fc1 = nn.Linear(latent_dim, nhid)
+        self.feat_fc2 = nn.Linear(nhid, data.num_features)
+        self.adj_fc1 = nn.Linear(latent_dim, nhid)
+        self.adj_fc2 = nn.Linear(nhid, data.num_nodes)
+        self.dropout = dropout
 
     def reset_parameters(self):
-        self.gc1.reset_parameters()
-        self.gc2.reset_parameters()
+        self.feat_fc1.reset_parameters()
+        self.feat_fc2.reset_parameters()
 
     def forward(self, z):
-        z = F.relu(self.gc1(z, self.inv_adj))
-        recon_x = self.gc2(z, self.inv_adj)
-        return torch.sigmoid(recon_x)
+        z = F.dropout(z, p=self.dropout, training=self.training)
+        a = F.relu(self.adj_fc1(z))
+        a = F.dropout(a, p=self.dropout, training=self.training)
+        recon_adj = self.adj_fc2(a)
+
+        x = F.relu(self.feat_fc1(z))
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        recon_feat = self.feat_fc2(x)
+        return recon_adj, recon_feat
